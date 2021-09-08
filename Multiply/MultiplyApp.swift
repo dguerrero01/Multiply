accede8ec24db4bdb9301b74511f52a7812c0c6c
Daisy Guerrero <dguerrero1@bsd220.org>
2021-09-08 10:55:16 -0500

    Initial Commit

---
+++ Multiply/MultiplyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplyApp.swift
+//  Multiply
+//
+//  Created by Student on 9/8/21.
+//
+
+import SwiftUI
+
+@main
+struct MultiplyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
